83d668e325754b0c6d3e305e5bc82ac51359a782
Bartolomeu Spegiorin Gusella <ccbarto@gmail.com>
2020-11-07 22:03:41 -0300

    layout em html da home

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,26 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
-  <router-view />
+  <main role="main" class="container">
+    <router-view />
+  </main>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  background-image: url(https://picsum.photos/650);
+  background-repeat: no-repeat;
+  background-size: 100% 40%;
+  min-height: 100%;
 }
 
 #nav {
+  text-align: center;
   padding: 30px;
 }
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,83 @@
 <template>
   <div class="home">
     <div class="row">
+      
       <div class="col-6">
-        <h1 class="p">Vida Toda</h1>
-        <h1></h1>
+        <div id="jumbo" class="m-3 p-4 border border-primary text-primary">
+          <h1 class="h6">Vida Toda</h1>
+          <h1 class="display-1 pb-5 font-weight-bold text-uppercase">Bem Estar</h1>
+          <h2>Soluções complestar para viver mais e melhor</h2>
+
+          <div class="row">
+            <div class="col text-center">
+              <button class="
+                text-white btn btn-lg btn-secondary 
+                mt-5 p-3 rounded-0">Quero uma consultoria</button>
+            </div>
+          </div>
+        </div>
+
+        <div class="m-3 p-4">
+          <h3 class="font-weight-bold text-secondary">Pense Nisso...</h3>
+          <p>O quanto o diagnóstico de uma doença grave pode afetar hoje, sua vida pessoal e financeira?</p>
+        </div>
+      </div>
+
+      <div class="col-6">
+        <div id="sideForm" class="bg-primary p-3 m-5 text-light ">
+          <h2 class="font-weight-bold text-secondary">Pronto para planejar seu sossego?</h2>
+          <p>Agende uma consultoria sem compromisso pelo formulário abaixo!</p>
+
+          <div class="form-group">
+            <label for="inp_name">Nome:</label>
+            <input type="text" id="inp_name" class="form-control"/>
+          </div>
+
+          <div class="form-group">
+            <label for="inp_email">Email:</label>
+            <input type="text" class="form-control" id="inp_email">
+          </div>
+
+          <div class="form-group">
+            <label for="inp_email">Estado Civil:</label>
+            <input type="text" class="form-control" id="inp_email">
+          </div>
+          
+          <div class="form-group">
+            <label for="inp_email">Profissão:</label>
+            <input type="text" class="form-control" id="inp_email">
+          </div>
+          
+          <div class="form-group">
+            <label for="inp_email">Telefone/WhatsApp:</label>
+            <input type="text" class="form-control" id="inp_email">
+          </div>
+          
+          <div class="form-group">
+            <label for="inp_email">Renda Mensal:</label>
+            <input type="text" class="form-control" id="inp_email">
+          </div>
+
+          <div class="row">
+            <div class="col text-center">
+              <button class="btn btn-lg btn-secondary 
+                rounded text-white font-weight-bold">Enviar</button>
+            </div>
+          </div>
+
+        </div>
       </div>
     </div>
   </div>
 </template>
-
+<style>
+#jumbo{
+  border-width: 5px !important;
+}
+#sideForm{
+  margin-top: 20rem !important;
+}
+</style>
 <script>
 // @ is an alias to /src
 //import HelloWorld from "@/components/HelloWorld.vue";
